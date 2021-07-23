764fcdc13d7659bc97ce40432642437ec935c740
Daniel Fernandes Rodrigues <61242535+Danielfr2405@users.noreply.github.com>
2021-07-22 21:34:15 -0300

    implementado para validar cors.

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import os
 from flask import Flask, request, redirect, render_template  # necessário instalar o flask (pip install -U Flask)
 from validaDados import findPattern # Importa a função findPattern que está dentro do arquivo validaDados.py
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+cors = CORS(app, resource={r"/*": {"origins": "*"}})
 
 
 @app.route("/")
@@ -16,5 +20,10 @@ def retorno():
     return "<h1> Hellow World!!! </h1>"
 
 
+def main():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 if __name__ == '__main__':
-    app.run()
+#     app.run()
+    main()
